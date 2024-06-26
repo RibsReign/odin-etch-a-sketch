66537831a75ac6997cb8d8682c09500b84640430
SkeleDrook <hulik.ondrej@gmail.com>
2024-06-26 01:23:07 +0000

    16x16 Grid, changed bottom-pasdding to height was causing issues with content

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 container = document.querySelector("div");
 
-for (let index = 0; index < 16; index++) {
-    divG = document.createElement("div");
+for (let index = 0; index < (16 * 16); index++) {
+    const divG = document.createElement("div");
     divG.setAttribute("class", "grid");
     container.appendChild(divG);
 }
