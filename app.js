2220bd218b54721ba2210c679a42b479ae5af509
SkeleDrook <hulik.ondrej@gmail.com>
2024-06-28 01:47:48 +0000

    Add Highlighting, Drawing and Grid Slider

--- app.js
+++ app.js
@@ -1,14 +1,55 @@
-container = document.querySelector("div");
+const container = document.querySelector("div");
 
-for (let index = 0; index < (16 * 16); index++) {
-    const tile = document.createElement("div");
-    tile.setAttribute("class", "grid");
-    tile.addEventListener("mouseover", (event) => {
+const sliderText = document.querySelector("p");
+const slider = document.querySelector("input");
+slider.addEventListener("change", () => {
+    gridCount = slider.value;
+    sliderText.textContent = `Grid size is ${slider.value}`;
+    makeGrid();
+})
+let gridCount = 16;
+
+let isDrawing = false;
+function colorOnHighlight(event) {
+    if (!isDrawing) {
         event.target.style.backgroundColor = "white";
-    })
-    tile.addEventListener("mouseout", (event) => {
+    }
+}
+function colorOnStart(event) {
+    if (!isDrawing) {
         event.target.style.backgroundColor = "blue";
-    })
-    container.appendChild(tile);
+    }
+}
+function mouseClick(event) {
+    isDrawing = !isDrawing;
+}
+function mouseLift(event) {
+    isDrawing = false;
+}
+function colorOnClick(event) {
+    if (isDrawing) {
+        event.target.style.backgroundColor = "black";
+        event.target.removeEventListener("mouseover", colorOnHighlight);
+        event.target.removeEventListener("mouseout", colorOnStart);
+    }
+
+}
+function makeGrid() {
+    container.innerHTML = '';
+    container.style.setProperty('--grid-count', gridCount);
+    for (let index = 0; index < (gridCount * gridCount); index++) {
+        const tile = document.createElement("div");
+        tile.setAttribute("class", "grid");
+        tile.addEventListener("mouseover", colorOnHighlight);
+        tile.addEventListener("mouseout", colorOnStart);
+        tile.addEventListener("mousedown", mouseClick);
+        document.addEventListener("mouseup", mouseLift);
+        tile.addEventListener("mousemove", colorOnClick);
+        tile.addEventListener('dragstart', function (event) {
+            event.preventDefault();
+        });
+        container.appendChild(tile);
+    }
 }
 
+makeGrid()
